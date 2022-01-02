d186ddd01fd222ab2d866316708cc82ee07c4f60
Olya Ocheretko <olyaocheretko@gmail.com>
2022-01-02 18:54:30 +0200

    Add vue-task-2(1,2,3 tasks)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
-
+  <div id="app" class="app">
+    <PostPage></PostPage>
+  </div>
 </template>
 
 <script>
+import PostPage from "./components/PostPage.vue"
 
+export default {
+  name: 'App',
+  data: () => ({
+  }),
+  components: {
+    PostPage
+  }
+}
 </script>
 
-<style>
-
+<style lang="scss">
+  *, *:before, *:after {box-sizing: border-box;}
+  html,body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    min-width: 320px;
+    position: relative;
+    color: #221f1f;
+  }
+  body {
+    line-height: 27px;
+    font-family: 'Lato', sans-serif;
+    font-size: 16px;
+    background-color: #f4f4f6;
+  }
+  .app {
+    max-width: 1200px;
+    padding: 0 20px;
+    margin: auto;
+    width: 100%;
+    text-align: center;
+  }
 </style>
---
+++ src/components/PostPage.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="post-page">
+    <div class="post">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+      <span>{{ formatedPostDate }}</span>
+      <hr/>
+    </div>
+
+    <div class="comments">
+      <div v-bind:id="`comment-${commentList.id}`" class="comments__item" v-for="commentList in commentsList" :key="commentList.id">
+        <h3>{{ commentList.name }}</h3>
+        <span>{{ commentList.email }}</span>
+        <p>{{ formatedCommentText(commentList.body) }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostPage',
+  data: () => ({
+    post: {
+     Id: 1,
+     title: 'Eum et est occaecati',
+     body: `Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velitullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velitullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     Ullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velitullam et saepe reiciendis voluptatem adipisci\nsit … ipsam iure\nquis sunt voluptatem rerum illo velit.
+     `,
+     postDate: new Date(),
+    },
+    commentsList: [
+      {
+        "postId": 1,
+        "id": 1,
+        "name": "id labore ex et quam laborum",
+        "email": "Eliseo@gardner.biz",
+        "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+      },
+      {
+        "postId": 1,
+        "id": 2,
+        "name": "quo vero reiciendis velit similique earum",
+        "email": "Jayne_Kuhic@sydney.com",
+        "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+      },
+      {
+        "postId": 1,
+        "id": 3,
+        "name": "odio adipisci rerum aut animi",
+        "email": "Nikita@garfield.biz",
+        "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+      },
+      {
+        "postId": 1,
+        "id": 4,
+        "name": "alias odio sit",
+        "email": "Lew@alysha.tv",
+        "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+      },
+      {
+        "postId": 1,
+        "id": 5,
+        "name": "vero eaque aliquid doloribus et culpa",
+        "email": "Hayden@althea.biz",
+        "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+      }
+    ],
+  }),
+  methods: {
+    formatedCommentText(baseString) {
+      let cutNumber = 20
+      let changedString = baseString[0].toUpperCase() + baseString.slice(1)
+      if(changedString.length > cutNumber) {
+        let shortString = changedString.slice(0, cutNumber - 1)
+        shortString = shortString + '...'
+
+        return shortString
+      }
+      return changedString
+    },
+  },
+  computed: {
+    formatedPostDate() {
+      let monthNames = [
+        'January', 'February', 'March',
+        'April', 'May', 'June', 'July',
+        'August', 'September', 'October',
+        'November', 'December'
+      ]
+      let date = this.post.postDate
+      //if need to set date in line with task
+      //date.setFullYear(2021, 11, 12);
+      let day = date.getDate()
+      let monthIndex = date.getMonth()
+      let year = date.getFullYear()
+
+      return `${day} ${monthNames[monthIndex]}, ${year}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .post-page {
+    background: #fff;
+    padding: 20px;
+    border-radius: 10px;
+  }
+  .comments {
+    text-align: left;
+    color: #7663cb;
+  }
+</style>
